d00e15d8e5c9c3fba504a5e848bd5e67bcaf2156
Fixxoo <rajpalmal40@gmail.com>
2024-08-12 22:33:24 +0530

    Under Maintainance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,9 @@
 import { Link } from "react-router-dom";
-import { PencilIcon, UserGroupIcon } from "@heroicons/react/24/solid";
+import { PencilIcon} from "@heroicons/react/24/solid";
 import Button from "./Button";
-import { account } from "../utils/appWrite";
-import { useState, useEffect } from "react";
 
 const Navbar = () => {
 
-	const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
-
-	const checkUser = async () => {
-		try {
-			const user = await account.get();
-			console.log('User is logged in:', user);
-			setIsLoggedIn(true)
-			return true;
-		} catch (error: any) {
-			if (error.message.includes('missing scope (account)')) {
-				return false;
-			} else {
-				throw error;
-			}
-		}
-	}
-
-	useEffect(() =>{
-      checkUser();
-	})
 
 	return (
 		<nav className="py-4 border-b-2 border-container shadow-md shadow-gray-400 w-full fixed top-0 bg-base">
